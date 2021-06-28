71d72f292f7bf14be133ba7529e0b9265db444b0
Lucas Magno <lucasmagno695@gmail.com>
2021-06-28 18:41:14 -0300

    Jogo da Forca

---
+++ forca.c
@@ -0,0 +1,9 @@
+#define TAMANHO_PALAVRA 20
+void abertura();
+int enforcou();
+void chuta() ;
+int jachutou(char letra);
+int ganhou();
+void desenhaforca();
+void escolhepalavra();
+int chuteserrados();
